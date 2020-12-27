12b92b2f0186640ddbc9d03e2db0fd77915827ff
Ishika Panwar <ishkipanwar@gmail.com>
2020-12-27 12:00:15 +0530

    icons

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -5,9 +5,9 @@ import {useParams } from "react-router-dom";
 
 function ChatRoom(){
     const { roomId } = useParams();
-    return <div className="chatRoom">
+    return (<div className="chatRoom">
         <h2>You are in the {roomId} room</h2>
-    </div>
+    </div>);
 }
 
 export default ChatRoom;
