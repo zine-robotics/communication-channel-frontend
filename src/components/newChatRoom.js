2d267a0f595c1034c8bf29704424c0fb276ce829
aryamanprince <2019uee1722@mnit.ac.in>
2020-12-30 22:46:51 +0530

    error fix try

--- src/components/newChatRoom.js
+++ src/components/newChatRoom.js
@@ -12,30 +12,229 @@ $(function(){
 
 
 function ChatRoom() {
+  var roomMessages= [
+    {
+        id: "5fe6e29931cf76666b0f702a",
+        senderId: "5fe5bc013fae5014fb8de01e",
+        content: "mes",
+        conversationId : "5fe5bb233fae5014fb8de01d",
+        createdAt: "2020-12-26T07:13:29.881Z",
+        updatedAt: "2020-12-26T07:13:29.881Z",
+        __v: 0
+    },
+    {
+        _id: "5fe6e29e31cf76666b0f702b",
+        senderId: "5fd2489d71c8f43084d6746d",
+        content: "mes",
+        conversationId: "5fe5bb233fae5014fb8de01d",
+        createdAt: "2020-12-26T07:13:34.804Z",
+        updatedAt: "2020-12-26T07:13:34.804Z",
+        __v: 0
+    },
+    {
+      _id: "5fe6e2f4a145d9670446f3b2",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "mes",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:15:00.169Z",
+      updatedAt: "2020-12-26T07:15:00.169Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e2f9a145d9670446f3b3",
+      senderId: "5fd2489d71c8f43084d6746d",
+      content: "mes",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:15:05.763Z",
+      updatedAt: "2020-12-26T07:15:05.763Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e366bb6f3867879f699c",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "mes",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:16:54.034Z",
+      updatedAt: "2020-12-26T07:16:54.034Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e369bb6f3867879f699d",
+      senderId: "5fd2489d71c8f43084d6746d",
+      content: "mes",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:16:57.692Z",
+      updatedAt: "2020-12-26T07:16:57.692Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e36cbb6f3867879f699e",
+      senderId: "5fd2489d71c8f43084d6746d",
+      content: "mesdasjasbdf",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:00.943Z",
+      updatedAt: "2020-12-26T07:17:00.943Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e36ebb6f3867879f699f",
+      senderId: "5fd2489d71c8f43084d6746d",
+      content: "mesdasjasbdf",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:02.144Z",
+      updatedAt: "2020-12-26T07:17:02.144Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e371bb6f3867879f69a0",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:05.243Z",
+      updatedAt: "2020-12-26T07:17:05.243Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e376bb6f3867879f69a1",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjc",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:10.684Z",
+      updatedAt: "2020-12-26T07:17:10.684Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e379bb6f3867879f69a2",
+      senderId: "5fd2489d71c8f43084d6746d",
+      content: "zcdzbh",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:13.965Z",
+      updatedAt: "2020-12-26T07:17:13.965Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a6bb6f3867879f69a3",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:58.446Z",
+      updatedAt: "2020-12-26T07:17:58.446Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a7bb6f3867879f69a4",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:59.082Z",
+      updatedAt: "2020-12-26T07:17:59.082Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a7bb6f3867879f69a5",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:59.464Z",
+      updatedAt: "2020-12-26T07:17:59.464Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a7bb6f3867879f69a6",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:17:59.987Z",
+      updatedAt: "2020-12-26T07:17:59.987Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a8bb6f3867879f69a7",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:00.168Z",
+      updatedAt: "2020-12-26T07:18:00.168Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a8bb6f3867879f69a8",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:00.320Z",
+      updatedAt: "2020-12-26T07:18:00.320Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a8bb6f3867879f69a9",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:00.472Z",
+      updatedAt: "2020-12-26T07:18:00.472Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a8bb6f3867879f69aa",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:00.620Z",
+      updatedAt: "2020-12-26T07:18:00.620Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a8bb6f3867879f69ab",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:00.782Z",
+      updatedAt: "2020-12-26T07:18:00.782Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3a8bb6f3867879f69ac",
+      senderId: "5fe5bc013fae5014fb8de01e",
+      content: "dkfcbsdjcjkbhj",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:00.887Z",
+      updatedAt: "2020-12-26T07:18:00.887Z",
+      __v: 0
+  },
+  {
+      _id: "5fe6e3aabb6f3867879f69ad",
+      senderId: "5fd2489d71c8f43084d6746d",
+      content: "csdkadjcbnsd",
+      conversationId: "5fe5bb233fae5014fb8de01d",
+      createdAt: "2020-12-26T07:18:02.666Z",
+      updatedAt: "2020-12-26T07:18:02.666Z",
+      __v: 0
+  }
+];
   return (
-    <div>
+    <div className="all">
       <div className="container bootstrap snippets bootdey">
         <div className="tile tile-alt" id="messages-main">
           <div className="ms-menu">
             <div className="ms-user clearfix">
-              <img
+              {/* <img
                 src="https://bootdey.com/img/Content/avatar/avatar1.png"
                 alt=""
                 className="img-avatar pull-left"
-              />
+              /> */}
               <div>
-                Signed in as <br /> m-hollaway@gmail.com
+                USERNAME
               </div>
             </div>
             <div className="p-15">
               <div className="dropdown">
-                <a
+                {/* <a
                   className="btn btn-primary btn-block"
                   href
                   data-toggle="dropdown"
                 >
                   Messages <i className="caret m-l-5" />
-                </a>
+                </a> */}
                 <ul className="dropdown-menu dm-icon w-100">
                   <li>
                     <a href>
@@ -201,15 +400,15 @@ function ChatRoom() {
                 <i className="fa fa-bars" />
               </div>
               <div className="pull-left hidden-xs">
-                <img
+                {/* <img
                   src="https://bootdey.com/img/Content/avatar/avatar2.png"
                   alt=""
                   className="img-avatar m-r-10"
-                />
+                /> */}
                 <div className="lv-avatar pull-left"></div>
-                <span>David Parbell</span>
+                <span className="roomname">avid Parbell</span>
               </div>
-              <ul className="ah-actions actions">
+              {/* <ul className="ah-actions actions">
                 <li>
                   <a href>
                     <i className="fa fa-trash" />
@@ -251,7 +450,7 @@ function ChatRoom() {
                     </li>
                   </ul>
                 </li>
-              </ul>
+              </ul> */}
             </div>
             <div className="message-feed media">
               <div className="pull-left">
