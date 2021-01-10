c89f0e3c813420cedcf30604754231a4925e0a70
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-01-10 15:58:05 +0530

    deploy

--- src/useConfig.js
+++ src/useConfig.js
@@ -1,2 +1,2 @@
-export const api = "http://localhost:3000/api/";
-export const socket_api = "http://localhost:3000/"
+export const api = "https://communication-channel-backend.herokuapp.com/api";
+export const socket_api = "https://communication-channel-backend.herokuapp.com/"
