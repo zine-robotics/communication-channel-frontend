4dc7c7d123ce34a232ab624f90c7d5ab04a31900
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-01-11 14:31:39 +0530

    localhost

--- src/useConfig.js
+++ src/useConfig.js
@@ -1,5 +1,5 @@
-export const api = "https://communication-channel-backend.herokuapp.com/api/";
-export const socket_api = "https://communication-channel-backend.herokuapp.com/"
+// export const api = "https://communication-channel-backend.herokuapp.com/api/";
+// export const socket_api = "https://communication-channel-backend.herokuapp.com/"
 
-// export const api = "http://localhost:3000/api/";
-// export const socket_api = "http://localhost:3000/"
+export const api = "http://localhost:3000/api/";
+export const socket_api = "http://localhost:3000/"
