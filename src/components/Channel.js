0ab1bc488acaf3a092a5f9d6e54ef3fa8dffb744
Ishika Panwar <ishkipanwar@gmail.com>
2021-01-12 18:23:37 +0530

    channel-avatars

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -2,11 +2,26 @@ import React from "react";
 import "./css/ChatRoom.css";
 import Avatar from "react-avatar";
 import Aero from "./css/images/A5.webp";
+import Help from "./css/images/help.webp";
+import IC from "./css/images/ic1.webp";
+import Algo from "./css/images/algo5.jpeg";
+import bme from "./css/images/bme.jpg";
+import bee from "./css/images/ee1.webp";
 
 function Channel({ name }) {
   function avatar(){
     if(name==="Aeromodelling")
       return <Avatar src={Aero} size="40" round />
+    else if(name==="Help")
+      return <Avatar src={Help} size="40" round />  
+    else if(name==="IC MCU and Sensors")
+      return <Avatar src={IC} size="40" round /> 
+    else if(name==="Algo Pseudo")
+      return <Avatar src={Algo} size="40" round /> 
+    else if(name==="BME")
+      return <Avatar src={bme} size="40" round />       
+    else if(name==="BEE")
+      return <Avatar src={bee} size="40" round />       
 
     else
     return <strong><Avatar name={name} size="40" round /></strong>
