2b917295f17d38fb0e798b437a8b7a0df9b43ef2
Ishika Panwar <ishkipanwar@gmail.com>
2021-01-12 16:15:20 +0530

    avatar-img

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./css/ChatRoom.css";
 import Avatar from "react-avatar";
+import Aero from "./css/images/A5.webp";
 
 function Channel({ name }) {
+  function avatar(){
+    if(name==="Aeromodelling")
+      return <Avatar src={Aero} size="40" round />
+
+    else
+    return <strong><Avatar name={name} size="40" round /></strong>
+  }
   return (
     <a className="list-group-item media" href="#">
       <div className="pull-left avatars">
-      <strong><Avatar name={name} size="40" round /></strong>
+      {/* <strong><Avatar name={name} size="40" round /></strong> */}
+      {avatar()}
       </div>
       <div className="media-body">
         <div className="list-group-item-heading channel-padding">{name}</div>
