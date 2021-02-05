e65b1172c290edfc941b057869533de7fc7a1064
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-02-05 18:06:20 +0530

    production commit

--- src/useConfig.js
+++ src/useConfig.js
@@ -1,5 +1,5 @@
 // export const api = "https://communication-channel-backend.herokuapp.com/api/";
 // export const socket_api = "https://communication-channel-backend.herokuapp.com/"
 
-export const api = "http://18.207.115.53:3002/api/";
-export const socket_api = "http://18.207.115.53:3002/"
+export const api = "http://18.207.115.53:3000/api/";
+export const socket_api = "http://18.207.115.53:3000/"
